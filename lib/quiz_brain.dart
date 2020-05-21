ed029ab5fbaab2ff93b007b123146f24209c9615
Paul P <paulandrewparker@gmail.com>
2020-05-21 14:01:19 -0400

    final

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -1,4 +1,5 @@
 import 'question.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 class QuizBrain {
   int _questionNumber = 0;
@@ -47,7 +48,24 @@ class QuizBrain {
 
   //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
 
-  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
 
-  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+      print('Now returning true');
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //TODO: Step 4 part B - Create a reset() method here that sets the questionNumber back to 0.
+  void reset() {
+    _questionNumber = 0;
+  }
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
 }
